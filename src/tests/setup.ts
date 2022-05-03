3c70673c94689fde445fc1b222e6328b6d55772e
Marcos Romeu <egaden87@gmail.com>
2022-05-03 17:31:15 +0200

    Ejemplo simple test

---
+++ src/tests/setup.ts
@@ -0,0 +1,4 @@
+import '@testing-library/jest-dom';
+
+// Necesario hasta futura actualización de testing-library
+globalThis.IS_REACT_ACT_ENVIRONMENT = true;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="vite/client" />
+
+// Necesario hasta futura actualización de testing-library
+declare  var IS_REACT_ACT_ENVIRONMENT: boolean;
